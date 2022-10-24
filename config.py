539d56905519b0aff5e2da046dc652ee493ba589
HYUN <feath3r@naver.com>
2022-10-24 15:36:44 +0900

    파이보 프로젝트 최초 커밋

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# ORM 적용하기 위한 데이터베이스 설정
+BASE_DIR = os.path.dirname(__file__)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "dev"
